cae45772cff2096686b6f5309d28f9f0233376c0
msomers87 <mara_somers@hotmail.com>
2023-11-12 08:46:54 +0100

    improved efficiency

--- project_medical_insurance/medical_insurance_analysis.py
+++ project_medical_insurance/medical_insurance_analysis.py
@@ -1,134 +1,55 @@
 import csv
 
-#convert csvfile into separate lists
-with open('C:/Users/SMR/data_analysis/project_medical_insurance/insurance.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    age = []
-    sex = []
-    bmi = []
-    children = []
-    smoker = []
-    region = []
-    charges = []
-    for row in reader:
-        age.append(row['age'])
-        sex.append(row['sex'])
-        bmi.append(row['bmi'])
-        children.append(row['children'])
-        smoker.append(row['smoker'])
-        region.append(row['region'])
-        charges.append(row['charges'])
+def read_csv(file_path):
+    data = {'age': [], 'sex': [], 'bmi': [], 'children': [], 'smoker': [], 'region': [], 'charges': []}
+    
+    with open(file_path) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            for key in data.keys():
+                data[key].append(row[key])
 
-#calculate average age
-sum_of_ages = 0
-for record in age:
-    sum_of_ages += int(record)
-average_age = round(sum_of_ages / len(age), 1)
+    return data
 
-#calculate count male and female
-male_count = 0
-female_count = 0
-for record in sex:
-    if record == 'male':
-        male_count += 1
-    elif record == 'female':
-        female_count += 1
+def calculate_average(data_list):
+    try:
+        return round(sum(map(float, data_list)) / len(data_list), 2)
+    except ZeroDivisionError:
+        return 0.0  # Handle the case where the list is empty
 
-#calculate average bmi
-sum_of_bmis = 0
-for record in bmi:
-    sum_of_bmis += float(record)
-average_bmi = round(sum_of_bmis / len(bmi), 1)
+def count_occurrences(data_list, value):
+    return data_list.count(value)
 
-#calculate average count of children
-sum_of_children = 0
-for record in children:
-    sum_of_children += int(record)
-average_children = round(sum_of_children / len(children), 1)
+if __name__ == "__main__":
+    file_path = 'C:/Users/SMR/data_analysis/project_medical_insurance/insurance.csv'
+    
+    data = read_csv(file_path)
 
-#calculate count smokers and nonsmokers.
-smoker_count = 0
-nonsmoker_count = 0
-for record in smoker:
-    if record == 'yes':
-        smoker_count += 1
-    elif record == 'no':
-        nonsmoker_count += 1
+    average_age = calculate_average(data['age'])
+    male_count = count_occurrences(data['sex'], 'male')
+    female_count = count_occurrences(data['sex'], 'female')
+    average_bmi = calculate_average(data['bmi'])
+    average_children = calculate_average(data['children'])
+    smoker_count = count_occurrences(data['smoker'], 'yes')
+    nonsmoker_count = count_occurrences(data['smoker'], 'no')
+    
+    # Count occurrences of regions
+    region_counts = {region: count_occurrences(data['region'], region) for region in set(data['region'])}
 
-#calculate count of regions
-southeast_count = 0
-southwest_count = 0
-northeast_count = 0
-northwest_count = 0
-for record in region:
-    if record == 'southeast':
-        southeast_count += 1
-    elif record == 'southwest':
-        southwest_count += 1
-    elif record == 'northeast':
-        northeast_count += 1
-    elif record == 'northwest':
-        northwest_count += 1
+    average_charges = calculate_average(data['charges'])
 
-#calculate average charges
-sum_of_charges = 0
-for record in charges:
-    sum_of_charges += float(record)
-average_charges = round(sum_of_charges / len(charges), 2)
-
-#print statements averages and counts:
-print("Average age: " + str(average_age))
-print("Average bmi: " + str(average_bmi))
-print("Average number of children: " + str(average_children))
-print("Average charges: " + str(average_charges))
-print("Number of males: " + str(male_count))
-print("Number of females: " + str(female_count))
-print("Number of smokers: " + str(smoker_count))
-print("Number of non-smokers: " + str(nonsmoker_count))
-print("Number of residents of region southeast: " + str(southeast_count))
-print("Number of residents of region southwest: " + str(southwest_count))
-print("Number of residents of region northeast: " + str(northeast_count))
-print("Number of residents of region northwest: " + str(northwest_count))
-
-#zip every list into 1 list
-medical_insurance_list = list(zip(age,sex,bmi,children,smoker,region,charges))
-
-#calculate average bmi per region
-list_regionSE = []
-list_regionSW = []
-list_regionNE = []
-list_regionNW = []
-for record in medical_insurance_list:
-    for item in record:
-        if item == "southeast":
-            list_regionSE.append(record)
-        elif item == "southwest":
-            list_regionSW.append(record)
-        elif item == "northwest":
-            list_regionNE.append(record)
-        elif item == "northeast":
-            list_regionNW.append(record)
-
-#sum_of_bmiSE = list_regionSE
-
-    #if record[5] == 'southeast':
-     #   sum_of_bmi_regionSE += float(record[2])
-    #elif record[5] == 'southwest':
-     #   sum_of_bmi_regionSW += float(record[2])
-    #elif record[5] == 'northeast':
-     #   sum_of_bmi_regionNE += float(record[2])
-    #elif record[5] == 'northwest':
-     #   sum_of_bmi_regionNW += float(record[2])
-
-#average_bmiSE = sum_of_bmi_regionSE / southeast_count
-#average_bmiSW = sum_of_bmi_regionSW / southwest_count
-#average_bmiNE = sum_of_bmi_regionNE / northeast_count
-#average_bmiNW = sum_of_bmi_regionNW / northwest_count
-
-#print("Average bmi for southeast: " + str(average_bmiSE) + ", Count = " + str(southeast_count))
-#print("Average bmi for southwest: " + str(average_bmiSW) + ", Count = " + str(southwest_count))   
-#print("Average bmi for northeast: " + str(average_bmiNE) + ", Count = " + str(northeast_count))
-#print("Average bmi for northwest: " + str(average_bmiNW) + ", Count = " + str(northwest_count)) 
+    # Output the results
+    print(f"Average Age: {average_age}")
+    print(f"Male Count: {male_count}")
+    print(f"Female Count: {female_count}")
+    print(f"Average BMI: {average_bmi}")
+    print(f"Average Children: {average_children}")
+    print(f"Smoker Count: {smoker_count}")
+    print(f"Non-Smoker Count: {nonsmoker_count}")
+    print("Region Counts:")
+    for region, count in region_counts.items():
+        print(f"{region}: {count}")
+    print(f"Average Charges: {average_charges}")
 
 
 
