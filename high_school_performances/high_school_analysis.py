7285c2a0b93abf4a3be20643ba0caa0fab9c4575
msomers87 <mara_somers@hotmail.com>
2023-12-05 21:33:34 +0100

    created function for t-test

--- high_school_performances/high_school_analysis.py
+++ high_school_performances/high_school_analysis.py
@@ -2,6 +2,11 @@
 from high_school_main import student_math, student_portuguese
 from scipy.stats import ttest_ind
 
+# Input for functions
+dataframe_input = student_math
+variable_input = 'final_grade'
+subgroup_input = 'address_type'
+
 # Function to calculate and print statistics for a given variable
 def calculate_and_print_stats(dataframe, variable):
     grades = dataframe[variable]
@@ -22,9 +27,9 @@ def calculate_and_print_stats(dataframe, variable):
     else:
         print(f"No mode found for {variable} (all students)")
 
-# Example usage
-calculate_and_print_stats(student_math, 'final_grade')
-
+# Print central tendencies of total dataset
+print("Central tendencies (total dataset):") 
+calculate_and_print_stats(dataframe_input, variable_input)
 
 # Calculate averages for subgroups
 def calculate_average_for_subgroups(dataframe, column_name, variable):
@@ -34,29 +39,30 @@ def calculate_average_for_subgroups(dataframe, column_name, variable):
         average = round(subset[variable].mean(), 1)
         print(f"Average {variable} for {column_name} = {value}: {average}")
 
-# Example usage
-calculate_average_for_subgroups(student_math, 'sex', 'final_grade')
+# Print central tendencies of subgroups
+print('Central tendencies (subgroups):')
+calculate_average_for_subgroups(dataframe_input, subgroup_input, variable_input)
 
+# Function for t-test
+def perform_t_test(dataframe, column_name, variable):
+    values = dataframe[column_name].unique()
+    subgroups = {value: dataframe[dataframe[column_name] == value][variable] for value in values}
+    
+    # Perform t-test between subgroups
+    for i in range(len(values)):
+        for j in range(i + 1, len(values)):
+            subgroup1 = values[i]
+            subgroup2 = values[j]
+            
+            t_stat, p_value = ttest_ind(subgroups[subgroup1], subgroups[subgroup2])
+            
+            print(f"T-test between {subgroup1} and {subgroup2}:")
+            print(f"T-statistic: {t_stat}, p-value: {p_value}")
+            
+            if p_value < 0.05:  # Assuming significance level of 0.05
+                print("The difference is statistically significant.")
+            else:
+                print("The difference is not statistically significant.")
 
-# Calculate t-test for Math
-grades_math_apart = student_math[student_math['parent_status'] == 'Apart']['final_grade']
-grades_math_together = student_math[student_math['parent_status'] == 'Living together']['final_grade']
-t_stat_math, p_value_math = ttest_ind(grades_math_apart, grades_math_together)
-
-# Calculate t-test for Portuguese
-grades_portuguese_apart = student_portuguese[student_portuguese['parent_status'] == 'Apart']['final_grade']
-grades_portuguese_together = student_portuguese[student_portuguese['parent_status'] == 'Living together']['final_grade']
-t_stat_portuguese, p_value_portuguese = ttest_ind(grades_portuguese_apart, grades_portuguese_together)
-
-# Print the results
-print(f"T-test results for Math:")
-print(f"T-statistic: {t_stat_math}")
-print(f"P-value: {p_value_math}")
-print("Statistically significant" if p_value_math < 0.05 else "Not statistically significant")
-
-print("T-test results for Portuguese:")
-print(f"T-statistic: {t_stat_portuguese}")
-print(f"P-value: {p_value_portuguese}")
-print("Statistically significant" if p_value_portuguese < 0.05 else "Not statistically significant")
-
-
+# Perform t-test for subgroups
+perform_t_test(dataframe_input, subgroup_input, variable_input)
--- high_school_performances/high_school_main.py
+++ high_school_performances/high_school_main.py
@@ -4,6 +4,8 @@ import pandas as pd
 student_math = pd.read_csv('C:/Users/SMR/data_analysis/high_school_performances/student_math_clean.csv')
 student_portuguese = pd.read_csv('C:/Users/SMR/data_analysis/high_school_performances/student_portuguese_clean.csv')
 
-
+# Checking general information 
+print(student_math.columns)
+print(student_math['address_type'].unique())
 
 
